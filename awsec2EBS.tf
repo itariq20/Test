dd0cce242d2b7707a64f5ba2afda12ed33af2173
irfantariq1997 <31933916+irfantariq1997@users.noreply.github.com>
2023-12-04 14:19:31 -0800

    Create awsec2EBS.tf
    test

---
+++ awsec2EBS.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+ebs_block_device {
+        device_name = "/dev/xvdba"
+        volume_size = 10
+        volume_type ="gp3"
+
+}
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
