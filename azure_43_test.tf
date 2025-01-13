31a9668b1cdd157f9b78d79f61b4f52f9c1e2ebf
itariq20 <31933916+itariq20@users.noreply.github.com>
2025-01-13 13:28:02 -0800

    Add files via upload

---
+++ azure_43_test.tf
@@ -0,0 +1,14 @@
+resource storageAccountResource 'Microsoft.Storage/storageAccounts@2023-01-01' = {
+  name: storageAccount.name
+  location: location
+  kind: 'StorageV2'
+  sku: {
+    name: 'Standard_GRS'
+  }
+  properties: {
+    allowBlobPublicAccess: false
+    minimumTlsVersion: 'TLS1_2'
+    supportsHttpsTrafficOnly: true
+  }
+  tags: tags
+}
