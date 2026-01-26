f715cf64dbd146e8a9f84ee3e96b26bea2526a08
itariq20 <31933916+itariq20@users.noreply.github.com>
2026-01-26 12:58:48 -0800

    Create iamtest.tf

---
+++ iamtest.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_user" "user" {
+  name          = "${local.resource_prefix.value}-user"
+  force_destroy = true
+
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-user"
+    Environment = local.resource_prefix.value
+    }, {
+    git_commit           = "5c6b5d60a8aa63a5d37e60f15185d13a967f0542"
+    git_file             = "terraform/aws/iam.tf"
+    git_last_modified_at = "2021-05-02 10:06:10"
+    git_last_modified_by = "nimrodkor@users.noreply.github.com"
+    git_modifiers        = "jonathan.jozwiak/nimrodkor"
+    git_org              = "itariq20"
+    git_repo             = "terragoat"
+    yor_trace            = "9b45b298-c1ea-426a-9644-610780021eaa"
+    }, {
+    yor_name = "user"
+  })
+
+}
+
+resource "aws_iam_access_key" "user" {
+  user = aws_iam_user.user.name
+}
+
+resource "aws_iam_user_policy" "userpolicy" {
+  name = "excess_policy"
+  user = "${aws_iam_user.user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*",
+        "s3:*",
+        "lambda:*",
+        "cloudwatch:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+output "username" {
+  value = aws_iam_user.user.name
+}
+
+output "secret" {
+  value = aws_iam_access_key.user.encrypted_secret
+}
